99e463f30d15374a2ea120d6de8c757bc08f4082
wengtiancheng <14786471+wengtiancheng@user.noreply.gitee.com>
2024-09-05 16:49:03 +0800

    将数字平方之后再排序，可以从大往小排，从左右两端看起，不管正负如何，平方之后最大的肯定在两侧

--- src/977.java
+++ src/977.java
@@ -7,6 +7,36 @@ class Solution {
             Double tmp = Math.pow(nums[i], 2);
             nums[i] = tmp.intValue();
         }
-        nums
+        for(int i = 0 ; i < len; i++){
+            for(int j = len - 2; j >= i; j--){
+                if(nums[j] > nums[j + 1]){
+                    int tmp =  nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = tmp;
+                }
+            }
+        }
+        return nums;
+    }
+    public int[] solution2(int[] nums) {
+        int len = nums.length;
+        int ptr = len - 1;
+        int[] tmp = new int[ptr + 1];
+        int left = 0;
+        int right = len - 1;
+        while(left <= right){
+            int ln = nums[left] * nums[left];
+            int rn = nums[right] * nums[right];
+            if(ln < rn){
+                tmp[ptr] = rn;
+                right--;
+            }else{
+                tmp[ptr] = ln;
+                left++;
+            }
+            ptr--;
+        }
+        return tmp;
+
     }
 }
